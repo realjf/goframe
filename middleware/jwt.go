4fb99f08ed998530def67fd3aec0b2ec560bb607
realjf <real_jf@hotmail.com>
2021-04-12 21:11:06 +0800

    update

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/dgrijalva/jwt-go"
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/realjf/goframe/utils"
 	"github.com/realjf/goframe/utils/conv"
